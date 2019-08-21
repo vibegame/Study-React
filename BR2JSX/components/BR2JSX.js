ff7822dc70c411f9533dcef3ff5fb2855d31b2ab
Nikita <46563672+V1be-Official@users.noreply.github.com>
2019-08-21 21:13:38 +0300

    jj

--- BR2JSX/components/BR2JSX.js
+++ BR2JSX/components/BR2JSX.js
@@ -4,9 +4,10 @@ class BR2JSX extends React.Component {
     renderWords = () => {
         let jsxCode = [];
         let arr = this.props.text.split(/<br\s*[\/]?>/gi);
-        for(let i = 0;i<arr.length;i++) {
+        for(let i = 0;i<arr.length - 1;i++) {
             jsxCode.push(arr[i], <br key={i}/>);
         }
+        jsxCode.push(arr[arr.length - 1]);
         return jsxCode;
     }
     render() {
