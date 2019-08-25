cd69d5e2f608a42e157c5e2124af55ff30de4b3d
Nikita <46563672+V1be-Official@users.noreply.github.com>
2019-08-25 23:47:11 +0300

    в

--- Rainbow/components/Rainbow.js
+++ Rainbow/components/Rainbow.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import RainbowFrame from './Frame';
+import {bindFrame} from './bindFrame';
 import "./Rainbow.css";
 class Rainbow extends React.Component {
     render() {
         let colors = ['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
-        return (<RainbowFrame colors={colors}>
+        let MainFrame = bindFrame(colors)(RainbowFrame);
+        return (<MainFrame>
             Hello!
-        </RainbowFrame>)
+        </MainFrame>)
     }
 }
 export default Rainbow;
