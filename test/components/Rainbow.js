58a9112a24ac16068b9d99ff7d79a991a509e9e2
Nikita <46563672+V1be-Official@users.noreply.github.com>
2019-08-31 18:58:10 +0300

    add

--- test/components/Rainbow.js
+++ test/components/Rainbow.js
@@ -4,6 +4,7 @@ import withRainbowFrame from './withRainbowFrame';
 import "./Rainbow.css";
 class Rainbow extends React.Component {
     render() {
+        console.log(withRainbowFrame);
         let colors = ['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
         let RainbowFrame = withRainbowFrame(colors)(Frame);
         return (
